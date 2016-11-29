5ea00f5e2415bb979ebd788df298f313b8e98a69
Nicolas <chnicolascel@gmail.com>
2016-11-29 17:30:05 +0100

    Calcule des points pour les lanes

---
+++ app/src/main/java/com/porte_ouverte/projetecole/Play/Starship.java
@@ -0,0 +1,17 @@
+package com.porte_ouverte.projetecole.Play;
+
+import android.content.Context;
+import android.graphics.drawable.ShapeDrawable;
+import android.widget.ImageView;
+
+/**
+ * Created by Nicolas on 29.11.2016.
+ */
+
+public class Starship extends ImageView {
+
+    public Starship(Context context) {
+        super(context);
+    }
+
+}
---
+++ app/src/main/java/com/porte_ouverte/projetecole/Utils/Helper.java
@@ -0,0 +1,23 @@
+package com.porte_ouverte.projetecole.Utils;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class Helper {
+
+    public static int pixelsToDp(int px, Context context) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, px,
+                context.getResources().getDisplayMetrics());
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+}
